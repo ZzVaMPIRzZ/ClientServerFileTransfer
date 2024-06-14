55e41c9be9e5c12b761dfc9b2fb8fd592377ee07
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2024-06-14 16:04:47 +0300

    фикс некоторых моментов

--- commands_for_build.sh
+++ commands_for_build.sh
@@ -4,3 +4,7 @@ venv/bin/pip install pyinstaller
 venv/bin/pip install tqdm
 venv/bin/pyinstaller --noconfirm --onefile --console "Client/client.py"
 venv/bin/pyinstaller --noconfirm --onefile --console "Server/server.py"
+rm -rf build
+rm -rf venv
+rm client.spec
+rm server.spec
